38e8f613354f6b75ac778f530c429adb84c24440
Kofi532 <kofi532@users.noreply.github.com>
2026-08-27 22:24:09 +0000

    Include deployable assessment assets

--- snakebite_care_gh/settings.py
+++ snakebite_care_gh/settings.py
@@ -152,6 +152,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'snakes_pics']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- snakebite_care_gh/urls.py
+++ snakebite_care_gh/urls.py
@@ -18,10 +18,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic.base import RedirectView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/venomguard/')),
+    path('', TemplateView.as_view(template_name='snakebite/splash.html'), name='splash'),
     path('venomguard/', include('snakebite.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
